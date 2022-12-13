7760fbb4bf2bd34a1a87bc11dfae5ed32d1b3d05
MatteoBottoo <115472031+MatteoBottoo@users.noreply.github.com>
2022-12-13 11:06:01 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+  
+   int n; 
+   cin>>n;
+   float a[n],m;
+  
+   for (int c=0;c<n;c++){
+       cin>>a[c];
+   }
+  
+   m=mass(n,a);
+   cout<<m;
+   return 0;
 }
