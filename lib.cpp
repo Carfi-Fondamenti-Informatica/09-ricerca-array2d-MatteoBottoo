ea0d2768320c1c5754455dc9df76f767635d7208
MatteoBottoo <115472031+MatteoBottoo@users.noreply.github.com>
2022-12-13 11:04:45 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,8 +1,13 @@
+#include "lib.h"
 
-#ifndef TUTORATO_21_11_22_LIB_H
-#define TUTORATO_21_11_22_LIB_H
-
-float mass(int n,float a[]);
-
-#endif //TUTORATO_21_11_22_LIB_H
-
+float mass(int n,float a[]){
+    float m;
+    for (int c=0;c<n;c++){
+        if(c==0){
+            m=a[c];
+        } else if (a[c]>m) {
+            m=a[c];
+        }
+    }
+    return m;
+}
