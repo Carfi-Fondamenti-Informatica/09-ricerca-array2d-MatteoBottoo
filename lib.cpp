c4fd4a0748bbd3561596263eaaa27f7e91664a94
MatteoBottoo <115472031+MatteoBottoo@users.noreply.github.com>
2022-12-17 11:26:50 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -2,11 +2,11 @@
 
 bool comp(char list[10][10], int &p, char name[]){
     for (int i=0;i<10;i++){
-        for (int j=0;j<20;j++){
-            if(list[i][j]!=name[j]){
-                j=20;
+        for (int k=0;k<20;k++){
+            if(list[i][k]!=name[j]){
+                k=20;
             }else{
-                if(j==19){
+                if(k==19){
                     p=i;
                     return true;
                 }
